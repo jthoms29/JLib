0356ea11980663dff99c63d5794d4c8de85314f1
jthoms29 <johnthoms567@gmail.com>
2025-07-13 13:43:34 -0600

    fleshed out documentation

--- JVEC/JVEC.h
+++ JVEC/JVEC.h
@@ -26,49 +26,77 @@ typedef struct JVEC {
     /// The current capacity of the vector
     size_t capacity;
 
-    // thread synchronization variables
+    /// Mutex variable used to access the vector in each function
     pthread_mutex_t vec_tex;
+
+    /// Condition variable. If a thread calls a function that modifies the
+    /// vector but there are currently other threads reading from the vector,
+    /// it will wait on this variable.
     pthread_cond_t vec_cond;
+
+    /// The number of threads currently reading from the vector. Since certain
+    /// functions don't modify the vector, multiple threads are able to call these
+    /// functions at a time.
     int readers;
 
 } JVEC;
 
 /**
  * Initializes a new JVEC. Returns a reference to the heap allocated vector.
- * @param[out] vector
  */
 JVEC* JVEC_new(void);
 
 
 /**
- * append a pointer to a variable to the end of the vector
+ * Append a variable to the end of the vector.
+ * \param[out] vector A pointer to a previously allocated JVEC
+ * \param[in] data_ptr Pointer to a piece of data you wish to append to the vector 
+ * \return 0 on success, 1 on failure
  */
 int JVEC_append(JVEC *vector, void* data_ptr);
 
 
 /**
- * Prepend a variable to the start of the vector
+ * Prepend a variable to the start of the vector.
+ * \param[out] vector A pointer to a previously allocated JVEC
+ * \param[in] data_ptr Pointer to a piece of data you wish to prepend to the vector 
+ * \return 0 on success, 1 on failure
  */
 int JVEC_prepend(JVEC *vector, void* data_ptr);
 
-/*
- * add a pointer to a variable to a valid index of the vector. If
- * an element already exists there, it is moved forward. If not (the first open position),
- * it is simply appended like normal.
+/**
+ * Add a pointer to a variable to a valid index of the vector. If
+ * an element already exists there, That element and all proceeding are moved forward.
+ * \param[out] vector A pointer to a previously allocated JVEC
+ * \param[in] data_ptr Pointer to a piece of data you wish to insert into to the vector 
+ * \return 0 on success, 1 on
  */
 int JVEC_insert_at(JVEC* vector, void* data_ptr, size_t index);
 
 /**
- * Get the element residing at the given index in the vector. Returns NULL on failure.
+ * Get the element residing at the given index in the vector.
+ * \param[out] vector A pointer to a previously allocated JVEC
+ * \param[in] index The index of the item in the vector you wish to retrieve
+ * \return A pointer to the item on success, NULL on failure.
  */
 void* JVEC_get_at(JVEC* vector, size_t index);
 
 
-
+/**
+ * Remove the element residing at the given index in the vector.
+ * \param[out] vector A pointer to a previously allocated JVEC
+ * \param[in] index The index of the item in the vector you wish to remove
+ * \return 0 on success, 1 on failure
+ */
 int JVEC_remove_at(JVEC *vector, void* data_ptr);
 
+/**
+ * Get the current length of the given vector.
+ \param[in] vector A pointer to a previously allocated JVEC
+ \return length on success, -1 on failure
+ */
+long JVEC_len(JVEC* vector);
 
-size_t JVEC_len(JVEC* vector);
 
 int JVEC_free(JVEC* vector);
 
