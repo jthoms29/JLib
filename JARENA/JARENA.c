1902e8c6fcad30199443fa68ac0c43f906296efe
jthoms29 <johnthoms567@gmail.com>
2026-04-03 08:04:09 -0600

    starting formal spec

--- JARENA/JARENA.c
+++ JARENA/JARENA.c
@@ -1,5 +1,21 @@
 #include <JARENA.h>
+#include <stdio.h>
 
+
+
+/*@
+    requires size_of_block > 0;
+    assigns \result \from size_of_block;
+
+    ensures \result == \null ||
+        (\valid(\result) &&
+        \valid(\result->mem_block + (0 .. size_of_block-1)) &&
+        \result->cur_block_address == \result->mem_block &&
+        \result->size == size_of_block);
+    
+    ensures \result != \null ==>
+        \initialized(\result->mem_block + (0 .. size_of_block-1));
+*/
 JARENA_NODE* JARENA_alloc_new_node(size_t size_of_block) {
     JARENA_NODE* new_node = calloc(1, sizeof(JARENA_NODE));
     if (!new_node) {
@@ -20,6 +36,17 @@ JARENA_NODE* JARENA_alloc_new_node(size_t size_of_block) {
     return new_node;
 }
 
+/*@
+    requires \true;
+    assigns \result;
+    allocates \result;
+    ensures \result == \null ||
+        (\valid(\result) &&
+        \result->head == \null &&
+        \result->tail == \null &&
+        \result->allocated_bytes == 0);
+
+ */
 JARENA* JARENA_new(void) {
     JARENA* new_arena = calloc(1, sizeof(JARENA));
     if (!new_arena) {
@@ -27,13 +54,23 @@ JARENA* JARENA_new(void) {
         return NULL;
     }
 
-
     new_arena->head = NULL;
     new_arena->tail = NULL;
     new_arena->allocated_bytes = 0;
     return new_arena;
 }
 
+
+/*@
+    requires arena != \null;
+    requires alloc_amount > 0;
+
+    assigns \result, arena->allocated_bytes;
+
+    ensures \result == \null ||
+        (\valid(\result + (0 .. alloc_amount-1)) &&
+         arena->allocated_bytes == \old(arena->allocated_bytes) + alloc_amount);
+*/
 byte* JARENA_alloc(JARENA* arena, size_t alloc_amount) {
 
     JARENA_NODE* walker = arena->head;
