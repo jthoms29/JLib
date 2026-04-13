023df14a3d0e3df84646623662a87e4b7a62636f
jthoms29 <johnthoms567@gmail.com>
2026-04-12 23:26:29 -0600

    cleaned up implementation, expanded unit tests

--- JHASHMAP/JHASHMAP.h
+++ JHASHMAP/JHASHMAP.h
@@ -8,86 +8,121 @@
 #include <stddef.h>
 #include <stdio.h>
 
-#define INITIAL_CAPACITY 1024 ///< Initial number of elements that can be held in map
-#define EMPTY  0x0
-#define IN_USE 0x1
-#define TOMB   0x2
+#define INITIAL_CAPACITY 1024 ///< Initial number of elements that can be held in map. MUST BE POWER OF 2!!
+#define EMPTY  0x0 // Indicates that an entry is empty
+#define IN_USE 0x1 // Indicates that an entry is currently in use
+#define TOMB   0x2 // Indicates that an entry was previously in use -> used for probing
 
-
-
+// used in the map resize function
 #define SHRINK 0
 #define GROW   1
 
-/**
- * An entry in the hashmap data structure. Holds key-value pair
+/*
+ * An entry in the hashmap data structure. Holds key-value pairs
  */
 typedef struct JHASHMAP_ENTRY {
-    /// Key for the entry, what will be used to retrieve the data
+    // Key for the entry, what will be used to retrieve the data
     void* key;
-    /// Value for the entry, data held
+    // Value for the entry, data held
     void* value;
+    // Current state of this entry. Macros defined above
     uint8_t state;
-    uint8_t pad[7];
 } JHASHMAP_ENTRY;
 
 
-/**
+/*
  * The hashmap data structure. Holds a vector of hashmap entries.
  */
 typedef struct JHASHMAP {
-    /// A pointer to a user defined hash function for whatever data-type the key is 
+    // A pointer to a user defined hash function for whatever data-type the key is 
     size_t (*hash_func)(void* key);
-
-    /// Function needed to compare keys of some data type
+    // Function needed to compare keys of some data type
     bool (*key_compare_func)(void* key1, void* key2);
-
-    /// The array that will hold the entries
+    // The array that will hold the entries
     JHASHMAP_ENTRY* vector;
-
-    /// the current number of occupied indices of the vector
+    // the current number of occupied indices of the vector
     size_t occupied;
-
-    /// the current capacity of the vector
+    // the current capacity of the vector
     size_t capacity;
-
-//    uint8_t* metadata;;
 } JHASHMAP;
 
 /**
- * Create a new hashmap. Must supply functions to hash keys and compare keys
- * @param[in] hash_func
- * @param[in] key_compare_func
- * \return A new heap allocated hashmap
+ * @brief Create a new hashmap.
+ * @details Allocates and returns a new JHASHMAP. 
+ * Functions to hash and compare keys must be supplied.
+ * @note Must be freed using JHASHMAP_free
+ * @param[in] hash_func Pointer to function used to hash keys
+ * @param[in] key_compare_func Pointer to function used to hash values
+ * @pre None
+ * @post New JHASHMAP allocated on heap
+ * @return Reference to new JHASHMAP
  */
 JHASHMAP* JHASHMAP_new(size_t (*hash_func) (void* key), bool (*key_compare_func) (void* key1, void* key2));
 
 /**
- * Add a key-value pair to the hashmap.
- * @param[in, out] map
- * @param[in] key
- * @param[in] value
- * \return 0 on success, anything else on failure
+ * @brief Add a key-value pair to the hashmap.
+ * @details Insert a value into the hashmap using the specified key.
+ * @note Key must be type that supplied hash function works with.
+ * @param[in, out] map JHASHMAP to be added to
+ * @param[in] key Key used to index supplied value
+ * @param[in] value Value to add to map
+ * @pre map, key must be non-null, key must be able to be hashed by supplied hash function
+ * @post Key value pair added to map. If pair with given key already exits,
+ * it is replaced. If pair is new, occupied count is incremented. If load
+ * factor increases enough from insertion, internal vector may resize
+ * @return 0 on success, anything else on failure
  */
 int JHASHMAP_add(JHASHMAP* map, void* key, void* value);
 
-
+/**
+ * @brief Remove key-value pair from hashmap
+ * @details Remove key-value pair that supplied key is associated
+ * with from hashmap.
+ * @note Key must be type that supplied hash function works with.
+ * @param[in, out] map JHASHMAP that pair will be removed from
+ * @param[in] key Key used to index pair to be removed
+ * @pre map, key must be non-null, key must be able to be hashed by supplied hash function
+ * @post If pair indexed by key exists in map, it is removed and associated value is returned.
+ * Occupied count decremented. If pair does not exist, nothing is done and NULL is returned
+ * @return value previously associated with key if pair exists in map, NULL otherwise
+ */
 void* JHASHMAP_remove(JHASHMAP* map, void* key);
 
 /** 
- * Get value from hashmap for corresponding key. Returns NULL if key
- * @param[in] map
- * @param[in] key
- * \return Value that corresponds to key, NULL if provided key not in map
+ * @brief Get value from hashmap for corresponding key
+ * @details Retrieves reference to value associated with key from map, if exists
+ * within map
+ * @note Key must be type that supplied hash function works with.
+ * @param[in] map JHASHMAP value will be retrieved from
+ * @param[in] key Key associated with value to be retrieved
+ * @pre map, key must be non-null, key must be able to be hashed by supplied hash function
+ * @post None
+ * @return Value that corresponds with key returned if pair exists in map,
+ * NULL otherwise.
  */
 void* JHASHMAP_get(JHASHMAP* map, void* key);
 
 /**
- * Checks if provided key is in map
- * @param[in] map
- * @param[in] key
+ * @brief Checks if provided key exists in map
+ * @details Returns boolean indicating if key is currently in map or not
+ * @note Key must be type that supplied hash function works with.
+ * @param[in] map JHASHMAP where existence of key will be checked
+ * @param[in] key Key to check existence of in map
+ * @pre map, key must be non-null, key must be able to be hashed by supplied hash function
+ * @post None
+ * @return true if key exists in map, false otherwise
  */
 bool JHASHMAP_has(JHASHMAP* map, void* key);
    
-
+/**
+ * @brief Free supplied JHASHMAP
+ * @details Frees internal vector of map as well as map itself.
+ * @note Data entered into map (keys, values) are NOT freed.
+ * @param[in, out] map_ptr Inderect pointer to allocated JHASHMAP
+ * @pre map_ptr must be indirect pointer to previously allocated JHASHMAP
+ * @post internal data structures and map itself are freed. Not data entered
+ * into map is freed
+ * @return None
+ */
 void JHASHMAP_free(JHASHMAP** map_ptr);
 #endif
--- JHASHMAP/test.c
+++ JHASHMAP/test.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <JHASHMAP.h>
 
 #include <stdio.h>
@@ -5,18 +6,65 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <string.h>
+#include <time.h>
 
+/* Get current time in milliseconds */
+long cur_ms() {
+    struct timespec ts;
+    if (clock_gettime(CLOCK_MONOTONIC, &ts)) {
+        perror("clock_gettime");
+    }
+    return ts.tv_sec * 1000 + ts.tv_nsec/1000000;
+}
 
+void rand_string(char* str, size_t len) {
+    const char charset[] =
+        "abcdefghijklmnopqrstuvwxyz"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "0123456789!";
 
-size_t string_hash(void* key, size_t map_capacity) {
-    unsigned char* str = (unsigned char*)key;
-    unsigned long hash = 5381;
+    for (size_t i = 0; i < len; i++) {
+        int idx = rand() % (sizeof(charset)-1);
+        str[i] = charset[idx];
+    }
+    str[len] = 0;
+}
+
+char** gen_unique_strings(size_t num, size_t strlens) {
+    char** strs = malloc(num * sizeof(char*));
+
+    for (size_t i = 0; i < num; i++) {
+        char rand_part[strlens-2];
+        rand_string(rand_part, sizeof(rand_part)-1);
+
+        int len = snprintf(NULL, 0, "%zu_%s", i, rand_part);
+        strs[i] = malloc(len+1);
+        // make sure unique
+        snprintf(strs[i], len + 1, "%zu_%s", i, rand_part);
+    }
+    return strs;
+}
+
+void free_unique_strings(char** strs, size_t num) {
+    for (size_t i = 0; i < num; i++) {
+        free(strs[i]);
+    }
+    free(strs);
+} 
+
+size_t string_hash(void* key) {
+    unsigned char* str = key;
+    size_t hash = 1469598103934665603ULL;
 
     while (*str) {
-        hash = ((hash << 5) + hash) + *str;
-        str++;
+        hash ^= (size_t)*str++;
+        hash *= 1099511628211ULL;
     }
-    return (size_t)(hash % map_capacity);
+    return hash;
+}
+
+size_t force_collide(void* key) {
+    return 21;
 }
 
 bool string_compare(void* key1, void* key2) {
@@ -29,93 +77,206 @@ void test_create_map(void) {
     assert(map != NULL);
     assert(map->occupied == 0);
     assert(map->capacity == INITIAL_CAPACITY);
-    JHASHMAP_free(map);
+    //JHASHMAP_free(&map);
     printf("test_create_map passed\n");
 }
 
 void test_free(void) {
     JHASHMAP* map = JHASHMAP_new(string_hash, string_compare);
-    assert(JHASHMAP_free(map) == 0);
+    //assert(JHASHMAP_free(map) == 0);
     printf("test_free passed\n");
 }
 
-void test_add_and_get(void) {
+void test_add_and_get(size_t num) {
+    printf("\nadd and get performance:\n");
+
     JHASHMAP* map = JHASHMAP_new(string_hash, string_compare);
 
-    char* key = "hello";
-    int value = 21;
+    char** keys = gen_unique_strings(num, 128);
+    char** vals = gen_unique_strings(num, 128);
 
-    assert(JHASHMAP_add(map, key, &value) == 0);
+    long tm, start = cur_ms();
+    // put these in the map
+    for (size_t i = 0; i < num; i++) {
+        assert(!JHASHMAP_add(map, keys[i], vals[i]));
+        // make sure load factor stays low as expected
+        assert(map->occupied == i+1);
+        assert((double)map->occupied / (double) map->capacity <= 0.5);
+    }
+    tm = cur_ms() - start;
+    printf("%ld insertions in %ld ms\n", num, tm);
 
-    int* result = (int*)JHASHMAP_get(map, key);
-    assert(result != NULL);
-    assert(*result == 21);
 
-    JHASHMAP_free(map);
-    printf("test_add_and_get passed\n");
+    // retrieve them
+    start = cur_ms();
+    for (size_t i = 0; i < num; i++) {
+        char* retrv = JHASHMAP_get(map, keys[i]);
+        assert(retrv != NULL);
+        assert((char*) retrv == vals[i]); // make sure proper val returned
+    }
+    tm = cur_ms() - start;
+    printf("%ld retrievals in %ld ms\n", num, tm);
+
+    free_unique_strings(keys, num);
+    free_unique_strings(vals, num);
+    JHASHMAP_free(&map);
 }
 
-void test_has(void) {
+void test_remove(size_t num) {
+    printf("\nremove performance:\n");
+
+    char** keys = gen_unique_strings(num, 128);
+    char** vals = gen_unique_strings(num, 128);
+
+    JHASHMAP* map = JHASHMAP_new(string_hash, string_compare);
+    for (size_t i = 0; i < num; i++) {
+        JHASHMAP_add(map, keys[i], vals[i]);
+    }
+    assert(map->occupied == num);
+    
+    long tm, start = cur_ms();
+    for (size_t i = 0; i < num; i++) {
+        assert(JHASHMAP_remove(map, keys[i]));
+        assert(map->occupied == num - (i+1));
+        assert(map->occupied < INITIAL_CAPACITY || (double)map->occupied / (double) map->capacity >= 0.25);
+    }
+    tm = cur_ms() - start;
+    printf("%ld removals in %ld ms\n", num, tm);
+
+    JHASHMAP_free(&map);
+    free_unique_strings(keys, num);
+    free_unique_strings(vals, num);
+
+}
+
+void test_collision(size_t num) {
+    printf("\ncollision performance:\n");
+
+    JHASHMAP* map = JHASHMAP_new(force_collide, string_compare);
+
+    char** keys = gen_unique_strings(num, 128);
+    char** vals = gen_unique_strings(num, 128);
+
+    long tm, start = cur_ms();
+    // put these in the map
+    for (size_t i = 0; i < num; i++) {
+        assert(!JHASHMAP_add(map, keys[i], vals[i]));
+        // make sure load factor stays low as expected
+        assert(map->occupied == i+1);
+        assert((double)map->occupied / (double) map->capacity <= 0.5);
+    }
+    tm = cur_ms() - start;
+    printf("%ld insertions in %ld ms\n", num, tm);
+
+
+    // retrieve them
+    start = cur_ms();
+    for (size_t i = 0; i < num; i++) {
+        char* retrv = JHASHMAP_get(map, keys[i]);
+        assert(retrv != NULL);
+        assert((char*) retrv == vals[i]); // make sure proper val returned
+    }
+    tm = cur_ms() - start;
+    printf("%ld retrievals in %ld ms\n", num, tm);
+
+    free_unique_strings(keys, num);
+    free_unique_strings(vals, num);
+    JHASHMAP_free(&map);
+}
+
+void test_tombstone(size_t num) {
+    printf("\ntombstone performance:\n");
+
+    JHASHMAP* map = JHASHMAP_new(force_collide, string_compare);
+
+    char** keys = gen_unique_strings(num, 128);
+    char** vals = gen_unique_strings(num, 128);
+
+    long tm, start = cur_ms();
+    // put these in the map
+    for (size_t i = 0; i < num; i++) {
+        assert(!JHASHMAP_add(map, keys[i], vals[i]));
+        // make sure load factor stays low as expected
+        assert(map->occupied == i+1);
+        assert((double)map->occupied / (double) map->capacity <= 0.5);
+    }
+    tm = cur_ms() - start;
+    printf("%ld insertions in %ld ms\n", num, tm);
+
+
+    // delete every second input
+    for (size_t i = 0; i < num/2; i++) {
+        assert(JHASHMAP_remove(map, keys[i*2]));
+        // now attempt getting the element immediately after
+        assert(JHASHMAP_get(map, keys[i*2+1]));
+    }
+    // retreive all inputs that still exist
+
+
+
+    tm = cur_ms() - start;
+    printf("%ld retrievals in %ld ms\n", num, tm);
+
+    free_unique_strings(keys, num);
+    free_unique_strings(vals, num);
+    JHASHMAP_free(&map);
+}
+
+void fuzz_test(size_t num, size_t iter) {
     JHASHMAP* map = JHASHMAP_new(string_hash, string_compare);
 
-    char* key1 = "apple";
-    char* key2 = "banana";
+    char** keys = gen_unique_strings(num, 64);
+    char** vals = gen_unique_strings(num, 64);
 
-    int v1 = 1;
-    JHASHMAP_add(map, key1, &v1);
+    printf("\nfuzz test performance\n");
+    size_t insertions = 0;
+    size_t deletions = 0;
+    size_t gets = 0;
 
-    assert(JHASHMAP_has(map, key1) == true);
-    assert(JHASHMAP_has(map, key2) == false);
+    long start = cur_ms();
 
-    JHASHMAP_free(map);
-    printf("test_has passed\n");
+    for (size_t i = 0; i < iter; i++) {
+        int op = rand() % 3;
+        int k = rand() % num;
+
+        char* key = keys[k];
+        char* val = vals[k];
+        char* ret;
+        switch (op) {
+            case 0:
+            JHASHMAP_add(map, key, val);
+            insertions++;
+            break;
+
+            case 1:
+            if ((ret = JHASHMAP_remove(map, key))) {
+                assert(ret == val);
+            }
+            deletions++;
+            break;
+
+            case 2:
+            if ((ret =JHASHMAP_get(map, key))) {
+                assert(ret == val);
+            };
+            gets++;
+            break;
+        }
+    } 
+    long tm = cur_ms() - start;
+
+    printf("%ld insertions, %ld deletions, %ld gets in %ld ms\n", insertions, deletions, gets, tm);
+    printf("%ld occupied, %ld capacity\n", map->occupied, map->capacity);
 }
-
-void test_overwrite_value(void) {
-    JHASHMAP* map = JHASHMAP_new(string_hash, string_compare);
-
-    char* key = "key";
-
-    int v1 = 10;
-    int v2 = 20;
-
-    JHASHMAP_add(map, key, &v1);
-    JHASHMAP_add(map, key, &v2);
-
-    int* result = (int*)JHASHMAP_get(map, key);
-    assert(result != NULL);
-    assert(*result == 20);
-
-    JHASHMAP_free(map);
-    printf("test_overwrite_value passed\n");
-}
-
-void test_multiple_entries_and_collisions(void) {
-    JHASHMAP* map = JHASHMAP_new(string_hash, string_compare);
-
-    int v1 = 1, v2 = 2, v3 = 3;
-
-    JHASHMAP_add(map, "a", &v1);
-    JHASHMAP_add(map, "b", &v2);
-    JHASHMAP_add(map, "c", &v3);
-
-    assert(*(int*)JHASHMAP_get(map, "a") == 1);
-    assert(*(int*)JHASHMAP_get(map, "b") == 2);
-    assert(*(int*)JHASHMAP_get(map, "c") == 3);
-
-    JHASHMAP_free(map);
-    printf("test_multiple_entries_and_collisions passed\n");
-}
-
 
 
 int main(void) {
-    test_create_map();
-    test_free();
-    test_add_and_get();
-    test_has();
-    test_overwrite_value();
-    test_multiple_entries_and_collisions();
+    //test_create_map();
+    test_add_and_get(10000);
+    test_remove(10000);
+    test_collision(10000);
+    test_tombstone(10000);
+    fuzz_test(10000, 10000000);
 
     printf("\nAll JHASHMAP tests passed\n");
     return 0;
