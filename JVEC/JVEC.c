8b6864e083d14bfcad0c48aaf8dfd37c24fb1df9
jthoms29 <johnthoms567@gmail.com>
2026-05-02 18:57:32 -0600

    fixed issues with in_after(), cleaning up now

--- JVEC/JVEC.c
+++ JVEC/JVEC.c
@@ -1,25 +1,36 @@
 #include <JVEC.h>
 #include <stddef.h>
 #include <string.h>
+#include <assert.h>
 
 
 static inline int JVEC_resize(JVEC* vec, enum resize_action act) {
-  
-
-    size_t cur_cap = vec->cap;
-    size_t cur_pad = vec->pad;
-
-    size_t new_cap = cur_cap;
-    size_t new_pad = cur_pad;
-
-    ptrdiff_t start_offset = vec->start - vec->head;
+    
+    // caluculate new capacity and initial pad sizes based on given action 
+    // new values initialized to current vals, then modified
+    size_t new_cap = vec->cap;
+    size_t old_pad = vec->start - vec->head;
+    size_t new_pad = old_pad;
 
     switch (act) {
-        case GROW_RIGHT: new_cap *= 2; break;
-        case SHRINK_RIGHT: new_cap /= 2; break;
-        case GROW_LEFT: new_pad *= 2; break;
-        case SHRINK_LEFT: new_pad /= 2; break;
+        case GROW_RIGHT: 
+            new_cap *= 2; 
+            break;
+        case SHRINK_RIGHT:
+            assert(new_cap >= INITIAL_CAP); // can't shrink beyond initial size
+            new_cap /= 2; 
+            break;
+        
+        // for pad, modify allocation amount, not current size
+        case GROW_LEFT: 
+            new_pad = vec->pad_alloc * 2; 
+            break;
+        case SHRINK_LEFT: 
+            assert(new_pad >= INITIAL_PAD); // can't shrink beyond initial size
+            new_pad = vec->pad_alloc / 2; 
+            break;
     }
+
 
     void** new_head = (void**) realloc(vec->head, (new_pad + new_cap) * sizeof(void*));
     if (!new_head) {
@@ -27,16 +38,15 @@ static inline int JVEC_resize(JVEC* vec, enum resize_action act) {
         return 1;
     }
 
-    void** old_start = new_head + start_offset;
     void** new_start = new_head + new_pad;
-
-    if (new_start != old_start) {
-        memmove(new_start, old_start, vec->length*sizeof(void*));
+    if (old_pad != new_pad) {
+        void** old_start = new_head + old_pad;
+        memmove(new_start, old_start, vec->len*sizeof(void*));
     }
     vec->head = new_head;
     vec->start = new_start;
     vec->cap = new_cap;
-    vec->pad = new_pad;
+    vec->pad_alloc = new_pad;
 
     return 0;
 }
@@ -59,9 +69,9 @@ JVEC* JVEC_new(void (*item_free_func)(void* item)) {
     }
 
     vec->head = array;
-    vec->length = 0;
+    vec->len = 0;
     vec->cap = INITIAL_CAP;
-    vec->pad = INITIAL_PAD;
+    vec->pad_alloc = INITIAL_PAD;
     // start address. Leave space at front to account for prepends
     vec->start = array + INITIAL_PAD;
     vec->free_func = item_free_func;
@@ -77,13 +87,13 @@ static inline int JVEC_append_INLINE(JVEC* vec, void* data) {
         return -1;
     }
     // grow vec if needed
-    if (vec->length == vec->cap) {
+    if (vec->len == vec->cap) {
         JVEC_resize(vec, GROW_RIGHT);
     }
 
     // set next open index to new element
-    *(vec->start + vec->length) = data;
-    vec->length++;
+    *(vec->start + vec->len) = data;
+    vec->len++;
 
     return 0;
 }
@@ -104,7 +114,7 @@ static inline int JVEC_prepend_INLINE(JVEC* vec, void* data) {
     vec->start--;
     *(vec->start) = data;
 
-    vec->length++;
+    vec->len++;
     vec->cap++;
     return 0;
 }
@@ -114,35 +124,56 @@ int JVEC_prepend(JVEC* vec, void* data) {
 
 int JVEC_in_after(JVEC* vec, void* data, size_t idx) {
     size_t len;
-    if (!vec || idx > (len = vec->length)) {
+    
+    if (!vec || idx >= (len = vec->len)) {
         return -1;
     }
     // only concerned about exact index being modified 
     idx++;
-    if (!vec || idx > len) {
-        return -1;
-    }
     if (idx == len) {
         return JVEC_append_INLINE(vec, data);
     }
 
-    size_t cap = vec->cap;
-    if (len == cap) {
-        JVEC_resize(vec, GROW_RIGHT);
+    // want to shift everything before the new item backwards, or everything
+    // after it forward - shift whichever has fewer
+    void** old_block;
+    void** new_block;
+    size_t block_len;
+
+    // fewer entries on left side, shift left
+    if (idx < len-idx) { // idx known to be leq to len, subtraction is fine
+        if (vec->start == vec->head) { 
+            JVEC_resize(vec, GROW_LEFT); 
+        }
+        old_block = vec->start;
+        new_block = old_block - 1;
+        block_len = idx;
+        memmove(new_block, old_block, block_len * sizeof(void*));
+        vec->start--;
+        vec->cap++;
+        vec->start[idx] = data;
+
+    }
+    // fewer entries on right side, shift right
+    else {
+        if (len == vec->cap) { 
+            JVEC_resize(vec, GROW_RIGHT); 
+        }
+        old_block = vec->start + idx;
+        new_block = old_block + 1;
+        block_len = len - idx;
+        memmove(new_block, old_block, block_len * sizeof(void*));
+        vec->start[idx] = data;
     }
 
-    void** start = vec->start;
-    memmove((start+idx)+1, start+idx, (len-idx) * sizeof(void*));
-
-    *(start+idx) = data;
-    vec->length++;
+    vec->len++;
 
     return 0;
 }
 
 int JVEC_in_before(JVEC* vec, void* data, size_t idx) {
     size_t len;
-    if (!vec || idx > (len = vec->length)) {
+    if (!vec || idx > (len = vec->len)) {
         return -1;
     }
     // size_t can't be neg
